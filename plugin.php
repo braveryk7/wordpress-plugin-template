8bc23b6c0fa3ebf73dafd361e6865d18525fce10
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2021-11-14 18:00:11 +0900

    refs #7 initial commit

---
+++ plugin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name:       WordPress Plugin Template
+ * Description:       Example block written with ESNext standard and JSX support – build step required.
+ * Requires at least: 5.8
+ * Requires PHP:      7.0
+ * Version:           0.1.0
+ * Author:            Ken-chan
+ * Author URI:        https://twitter.com/braveryk7
+ * Plugin URI:        https://www.braveryk7.com/
+ * Text Domain:       wordpress-plugin-template
+ * Domain Path:       /languages
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * @author            Ken-chan
+ * @package           create-block
+ * @since             0.1.0
+ */
